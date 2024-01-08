8df3fb6fdd614ba888f2ffc85a57660997ef585c
Nilaxi <nilaximakwana@gmail.com>
2024-01-08 08:48:06 +0530

    AddToCartRedux

---
+++ src/reducer.js/main.js
@@ -0,0 +1,8 @@
+
+import { combineReducers } from "redux";
+import updateCart from "./update";
+const rootReducer=combineReducers({updateCart})
+
+
+
+export default rootReducer
---
+++ src/store/store.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+import rootReducer from "../reducer.js/main";
+const store=createStore(rootReducer)
+
+export default store
